a19d1109446399c6ffc77b5d0e435e3cf54a263c
vepaks <kamburoff@gmail.com>
2025-06-05 21:21:23 +0300

    add network to hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,4 +3,17 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {},
+    localdevnet: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+      chainId: 1337,
+    },
+  },
 };
